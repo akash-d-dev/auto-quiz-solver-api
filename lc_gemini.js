32e60b13a7dfc395e6dd120eaa5f6631011e399a
Akash Singh <ak45hhere@gmail.com>
2024-04-23 10:10:57 +0530

    prompt fix

--- lc_gemini.js
+++ lc_gemini.js
@@ -11,7 +11,7 @@ const { RunnableSequence } = require("@langchain/core/runnables");
 async function lc_gemini(qna, key, model = "gemini-1.0-pro") {
   const parser = new CommaSeparatedListOutputParser();
 
-  const instructions = `You are an expert quiz solver. You are provided with a quiz {subject}. The quiz is in a format similar to of list objects, where in each object has three properties - "question", "question_number" and "options". The "question" property contains the question, "question_number" contains the serial number of the question and "options" contains an array of objects where each object has two properties - "content" and "option_number". The "content" property contains the option and "option_number" contains the serial number of the option. You have to pick the correct answers using the "option_number" in the same order as the questions were given.
+  const instructions = `You are an expert quiz solver. You are provided with a quiz. The quiz is in a format similar to of list objects, where in each object has three properties - "question", "question_number" and "options". The "question" property contains the question, "question_number" contains the serial number of the question and "options" contains an array of objects where each object has two properties - "content" and "option_number". The "content" property contains the option and "option_number" contains the serial number of the option. You have to pick the correct answers using the "option_number" in the same order as the questions were given.
 
   Your response should be a list of comma separated values, eg: "0,3,2,1,1"
   
