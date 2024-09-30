100dbf57df5342b8444b009423fb9f94040889ad
akash-d-dev <ak45hhere@gmail.com>
2024-09-30 13:13:00 +0530

    chore: Update lc_gemini.js instructions for quiz response format

--- lc_gemini.js
+++ lc_gemini.js
@@ -15,7 +15,7 @@ async function lc_gemini(qna, key, model = "gemini-1.0-pro") {
 
   const instructions = `You are an expert quiz solver. You are provided with a quiz. The quiz is in a format similar to of list objects, where in each object has three properties - "question", "question_number" and "options". The "question" property contains the question, "question_number" contains the serial number of the question and "options" contains an array of objects where each object has two properties - "content" and "option_number". The "content" property contains the option and "option_number" contains the serial number of the option. You have to pick the correct answers using the "option_number" in the same order as the questions were given.
 
-  Your response should be a list of comma separated values, eg: "0,3,2,1,1"
+  Your response should be a list of comma separated values, eg: "0,3,2,1,1". Your response should contain only options numbers and should be in the same order as the questions were given. NOTHING OTHER THAN THE OPTION NUMBER SHOULD BE PRESENT IN YOUR RESPONSE.
   
   NOTE: Your correct option number of corect answer cannot be greater than 3. So your list of comma separated values should be in the range of 0-3. Also there will be 5 questions in the quiz, so your response should contain EXACTLY 5 comma separated values.
   `;
